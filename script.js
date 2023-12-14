e8e7ad588bc792e22b1bf9d7951eb9510f19dcb8
asha9089 <143885866+asha9089@users.noreply.github.com>
2023-12-14 00:08:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,69 @@
-function myFunction() {
-  var x = document.getElementById("screen1");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+document.addEventListener("DOMContentLoaded", function () {
+    // Load prior scores from local storage when the page loads
+    loadPriorScores();
+});
+
+function startGame() {
+    document.getElementById('play').classList.add('hidden');
+    document.getElementById('guess').classList.remove('hidden');
+    Game();
+    
+        }
+
+
+function checkGuess() {
+    const userGuess = parseInt(document.getElementById('guessInput').value);
+    const randomNumber = parseInt(localStorage.getItem('randomNumber'));
+
+    if (userGuess === randomNumber) {
+        document.getElementById('result').innerText = "Congratulations! you got it.";
+        saveScore(userGuess);
+    } else {
+        document.getElementById('result').innerText = "oops try again.";
+        if (userGuess > randomNumber) {
+            document.getElementById('result').innerText = "too high.";
+            saveScore(userGuess);
+        } else {document.getElementById('result').innerText = "too low.";
+
+        }}
+    }
+function Game() {
+    
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    localStorage.setItem('randomNumber', randomNumber);
 }
- // get screens, if display is none block what appears
-function functionn(){
-  var x = document.getElementById("screen2"); 
-  if (x.style.display === "none") {
-  x.style.display = "block";
-} else {
-  x.style.display = "none";
+
+
+
+function saveScore(score) {
+    const priorScores = JSON.parse(localStorage.getItem('priorScores')) || [];
+    priorScores.push(score);
+    localStorage.setItem('priorScores', JSON.stringify(priorScores));
 }
+
+function loadPriorScores() {
+    const scoresList = document.getElementById('scoresList');
+    scoresList.innerHTML = '';
+
+    const priorScores = JSON.parse(localStorage.getItem('priorScores')) || [];
+    priorScores.forEach(score => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Score: ${score}`;
+        scoresList.appendChild(listItem);
+    });
 }
 
- // The number to guess
- const correctNumber = 5;
+function showScores() {
+    document.getElementById('guess').classList.add('hidden');
+    document.getElementById('scores').classList.remove('hidden');
+    loadPriorScores();
+}
 
- function checkRandom() {
-     // Get the user's guess
-     const userRandom = document.getElementById('Random').value;
-
-     // Check if the guess is correct
-     if (userRandom == correctNumber) {
-         showMessage('Congratulations! You guessed it right.');
-     } else if (userRandom < correctNumber) {
-         showMessage('Too low, Keep trying.');
-     } else {
-         showMessage('Too high. keep trying.');
-     }
-
- }
-
- 
-
- function showMessage(message) {
-     // Display the  message
-     document.getElementById('message').textContent = message;
-     
+function hideScores() {
+    document.getElementById('guess').classList.add('hidden');
+    document.getElementById('scores').classList.add('hidden');
+}
+function resetGame() {
+    document.getElementById("play").classList.remove("hidden");
+    document.getElementById("scores").classList.add("hidden");
 }
